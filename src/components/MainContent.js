83715bab99c3daa2c37a9571fd0e78779ae61191
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-29 17:35:19 +0100

    Completed assignment

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,16 @@
 import React from "react";
+import TopMenu from "./TopMenu";
+import GenrePicker from "./GenrePicker";
+import AlbumList from "./AlbumList";
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return (
+    <main>
+      <TopMenu />
+      <GenrePicker />
+      <AlbumList />
+    </main>
+  );
 }
 
 export default MainContent;
